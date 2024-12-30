9e7c730f5a434851124fc50bc820d8f1c5095bb9
Parwiz Mohammadi <parwizmohammadi1234@gmail.com>
2024-12-30 15:37:37 +0330

    Id

--- Add.java
+++ Add.java
@@ -1,5 +1,8 @@
 class Add {
     public static void main(String[] args) {
         System.out.println("hello world");
+        for (int i = 0; i < 12; i++) {
+            System.out.println(12);
+        }
     }
 }
