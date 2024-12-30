2ef178c1194529174691d048e7ea600aff59a752
Parwiz Mohammadi <parwizmohammadi1234@gmail.com>
2024-12-30 17:47:44 +0330

    night_commit

--- Add.java
+++ Add.java
@@ -1,21 +1,15 @@
 class Add {
+
     public static void main(String[] args) {
         System.out.println("hello world");
         for (int i = 0; i < 12; i++) {
             System.out.println(12);
         }
-        int i = 12;
-        i += 1;
-        /*
-         * 
-         * sdkajsdhsklf
-         * fskf
-         * shkdf
-         * sdfhk
-         * sdf
-         * fs
-         * sdf
-         * df
-         */
+
+        System.out.println(i);
+        for (int i = 0; i < 1; i++) {
+            System.out.println(123);
+        }
+
     }
 }
